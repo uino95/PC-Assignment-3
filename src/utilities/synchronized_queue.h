453724d7a66fe0e1b54bfb0c454c2521719fa4fb
Matteo Sinico <sinicomatteo@gmail.com>
2018-10-15 12:10:30 +0200

    prettify the syntax

--- src/utilities/synchronized_queue.h
+++ src/utilities/synchronized_queue.h
@@ -3,40 +3,44 @@
 #include <list>
 #include <mutex>
 template<typename T>
-class SynchronizedQueue {
+class SynchronizedQueue
+{
 public:
-SynchronizedQueue(){}
-void put(const T & data);
-T get();
-int size();
+    SynchronizedQueue() {}
+    void put(const T &data);
+    T get();
+    int size();
 
-int size() {
-  return queue.size();
-}
+    int size()
+    {
+        return queue.size();
+    }
 private:
-SynchronizedQueue(const SynchronizedQueue &)=delete;
-SynchronizedQueue & operator=(const SynchronizedQueue &)=delete;
-std::list<T> queue;
-std::mutex myMutex;
-// std::condition_variable myCv;
+    SynchronizedQueue(const SynchronizedQueue &) = delete;
+    SynchronizedQueue &operator=(const SynchronizedQueue &) = delete;
+    std::list<T> queue;
+    std::mutex myMutex;
+    // std::condition_variable myCv;
 };
 
 template<typename T>
-void SynchronizedQueue<T>::put(const T & data)
+void SynchronizedQueue<T>::put(const T &data)
 {
-std::unique_lock<std::mutex> lck(myMutex);
-queue.push_back(data);
-//myCv.notify_one();
+    std::unique_lock<std::mutex> lck(myMutex);
+    queue.push_back(data);
+    //myCv.notify_one();
 }
 template<typename T>
-T SynchronizedQueue<T>::get() {
-for(;;) {
-std::unique_lock<std::mutex> lck(myMutex);
-if(queue.empty()) continue;
-T result=queue.front();
-queue.pop_front();
-return result;
-}
+T SynchronizedQueue<T>::get()
+{
+    for(;;)
+    {
+        std::unique_lock<std::mutex> lck(myMutex);
+        if(queue.empty()) continue;
+        T result = queue.front();
+        queue.pop_front();
+        return result;
+    }
 }
 
 #endif // SYNC_QUEUE_H_
